720177ff63da64f62ceb76663e83e342d6930a31
aakashshankar <aakashshankar217@gmail.com>
2024-06-03 23:38:09 -0400

    you can set default models now

--- defaults/model_configs.go
+++ defaults/model_configs.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 //go:embed static/model_configs.json
@@ -30,4 +31,25 @@ func GetDefaultModel(variant string) (string, error) {
 		return "", fmt.Errorf("unknown LLM type: %s", variant)
 	}
 	return defaultModels, nil
+}
+
+func SetDefaultModel(variant string, model string) error {
+	modelConfig.DefaultModels[variant] = model
+	// write model config to disk
+	updatedConfig, err := json.MarshalIndent(modelConfig, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshaling model config to JSON: %w", err)
+	}
+	exePath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("error getting executable path: %w", err)
+	}
+	// there's gotta be a better way to do this
+	configFile := filepath.Join(filepath.Dir(exePath), "../defaults/static/model_configs.json")
+
+	err = os.WriteFile(configFile, updatedConfig, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing updated model config to disk: %w", err)
+	}
+	return nil
 }
