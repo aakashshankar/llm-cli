ad67f21723456d5d4de5a1e882a40a0bc5e613ae
aakashshankar <aakashshankar217@gmail.com>
2024-08-03 20:48:16 -0400

    enable multiple session support. you can now inspect, switch and list sessions. a new session is created when you decid to start a fresh conversation with an llm

--- cmd/session.go
+++ cmd/session.go
@@ -6,7 +6,61 @@ import (
 )
 
 func init() {
-	sessionCmd := &cobra.Command{
+	sessionCmd := sessionCommand()
+	listCmd := listSessionCommand()
+	inspectCmd := inspectSessionCommand()
+	switchCmd := switchSessionCommand()
+
+	sessionCmd.AddCommand(listCmd)
+	sessionCmd.AddCommand(inspectCmd)
+	sessionCmd.AddCommand(switchCmd)
+
+	rootCmd.AddCommand(sessionCmd)
+}
+
+func inspectSessionCommand() *cobra.Command {
+	var yaml bool
+
+	inspectCmd := &cobra.Command{
+		Use:   "inspect <uuid>",
+		Short: "Inspect a session",
+		Long:  "Inspect a session",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			session.InspectSession(args[0], yaml)
+		},
+	}
+
+	inspectCmd.Flags().BoolVarP(&yaml, "yaml", "y", false, "Output in YAML format")
+
+	return inspectCmd
+}
+
+func switchSessionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "switch <uuid>",
+		Short: "Switch to a session",
+		Long:  "Switch to a session",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			session.SwitchSession(args[0])
+		},
+	}
+}
+
+func listSessionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Short: "List all sessions",
+		Long:  "List all sessions",
+		Run: func(cmd *cobra.Command, args []string) {
+			session.ListSessions()
+		},
+	}
+}
+
+func sessionCommand() *cobra.Command {
+	return &cobra.Command{
 		Use:   "session",
 		Short: "Manage existing sessions",
 		Long:  "Switch between existing sessions",
@@ -17,16 +71,4 @@ func init() {
 			}
 		},
 	}
-
-	listCmd := &cobra.Command{
-		Use:   "list",
-		Short: "List all sessions",
-		Long:  "List all sessions",
-		Run: func(cmd *cobra.Command, args []string) {
-			session.ListSession()
-		},
-	}
-
-	sessionCmd.AddCommand(listCmd)
-	rootCmd.AddCommand(sessionCmd)
 }
